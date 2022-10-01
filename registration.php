c920e4ea6f7bce17f77df2308764581dedd92cc1
kallard <contact@allard-kevin.fr>
2022-10-01 23:44:17 +0200

    01 - Create module

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+// app/code/Tutorial/Dog/registration.php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Tutorial_Dog',
+    __DIR__
+);
